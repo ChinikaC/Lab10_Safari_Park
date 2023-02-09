f023cbec6d058918b1d87f53cc2f39868415b106
Chinika charles <chinikacharles@chinikas-mbp.home>
2023-02-09 15:37:59 +0000

    started inserting our values

--- safaripark.sql
+++ safaripark.sql
@@ -11,6 +11,10 @@ CREATE TABLE enclosures(
     closedForMaintenance BOOLEAN
 );
 
+INSERT INTO enclosures(name, capacity,closedForMaintenance) VALUES ('Big Cat Field', 30, true);
+INSERT INTO enclosures(name, capacity,closedForMaintenance) VALUES ('Moneky House', 40, false);
+INSERT INTO enclosures(name, capacity,closedForMaintenance) VALUES ('Vivarium', 35, true);
+
 CREATE TABLE animals(
     id SERIAL PRIMARY KEY,
     name VARCHAR(40),
@@ -19,11 +23,25 @@ CREATE TABLE animals(
     enclosures_id INT REFERENCES enclosures(id)
 );
 
+INSERT INTO animals(name, type, age, enclosures_id) VALUES ('Tony', 'Bengal Tiger', 16);
+INSERT INTO animals(name, type, age, enclosures_id) VALUES ('Simba', 'Congo Lion', 8);
+INSERT INTO animals(name, type, age, enclosures_id) VALUES ('Mbaku', 'Silver Back Gorilla', 10);
+INSERT INTO animals(name, type, age, enclosures_id) VALUES ('George', 'Golden Sunb Nosed Monkey', 12);
+INSERT INTO animals(name, type, age, enclosures_id) VALUES ('Yoshi', 'Komodo Drago', 14);
+INSERT INTO animals(name, type, age, enclosures_id) VALUES ('Malfoy', 'Rock Python', 6);
+
 CREATE TABLE staff(
 id SERIAL PRIMARY KEY,
 name VARCHAR(40),
 employeeNumber INT
 );
+
+INSERT INTO staff(name, employeeNumber) VALUES ('Ed', 068);
+INSERT INTO staff(name, employeeNumber) VALUES ('Edward', 001);
+INSERT INTO staff(name, employeeNumber) VALUES ('Eddy', 054);
+INSERT INTO staff(name, employeeNumber) VALUES ('Edbert', 007);
+INSERT INTO staff(name, employeeNumber) VALUES ('Edwin', 013);
+INSERT INTO staff(name, employeeNumber) VALUES ('Eddison', 026);
 
 CREATE TABLE assignment(
 id SERIAL PRIMARY KEY,
@@ -31,3 +49,7 @@ employee_id INT REFERENCES staff(id),
 enclosures_id INT REFERENCES enclosures(id),
 day VARCHAR(20)
 );
+
+INSERT INTO assignment(employee_id, enclosures_id, day) VALUES ()
+INSERT INTO assignment(employee_id, enclosures_id, day) VALUES ()
+INSERT INTO assignment(employee_id, enclosures_id, day) VALUES ()
